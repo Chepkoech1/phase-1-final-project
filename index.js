8e54e947295cd06f25c42317c1b7f9243d184907
Sandra <sandrachepkoech60@gmail.com>
2026-02-26 16:09:42 +0300

    Refactored quiz logic and remove unused functions

--- index.js
+++ index.js
@@ -1,115 +1,68 @@
-getQuestions();
+let questions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+let selectedAnswer = null;
 
-let userAnwers = {};
-let position = 1;
-let number = 1;
+// Fetch questions from db.json
+fetch("db.json")
+    .then(response => response.json())
+    .then(data => {
+        questions = data.results;
+        showQuestion();
+    });
 
-
-async function getQuestions() {
-    await fetch('https://phase-1-final-project-six.vercel.app/db/db.json')
-        .then(res => res.json())
-        .then(res => {   
-            const question_text = document.querySelector('.question-text');
-            console.log(res.results);
-            res.results.forEach((element, index) => {
-                const p = document.createElement('p')
-                p.innerHTML = element.question;
-                if (index === 0) {
-                    p.classList.add('question_active')
-                }
-                question_text.append(p);
-                const totalQuiz = document.querySelector('.totalQuestions');
-                totalQuiz.textContent = res.results.length;
-                const question_position = document.querySelector('.question-position');
-                question_position.textContent = 1;
-            });
-        })
-        .catch(err => console.log(err.message))
-    
-    goToNext();
-    goToPrevious();
-    getAnswers(position);
-    getAnswers(position);
-    getAnswers(position);
-    getAnswers(position);
-    
+function decodeHTML(html) {
+    let txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
 }
 
-function goToNext() {
-    const btn = document.querySelector('.btn-next');
-    const questions = document.querySelectorAll('.question-text p')
-    btn.addEventListener('click', function () {
-        for (let index = 0; index < questions.length; index++){
-            if (questions[index].classList.contains('question_active')) {
-                const question_position = document.querySelector('.question-position');
-                position = parseInt(question_position.textContent) + 1;
-                questions[index].classList.remove('question_active');
-                if (index === questions.length - 1) {
-                    questions[0].classList.add('question_active');
-                    position = 1;
-                    
-                }
-                else {
-                    questions[index + 1].classList.add('question_active');
-                    
-                }
-                question_position.textContent = position;
-                break;
-                
-            }
-        }
+function showQuestion() {
+    const questionElement = document.getElementById("question");
+    const currentQuestion = questions[currentQuestionIndex];
 
-    })
-    
+    questionElement.innerText = decodeHTML(currentQuestion.question);
 }
 
-function goToPrevious() {
-    const btn = document.querySelector('.btn-previous');
-    const questions = document.querySelectorAll('.question-text p')
-    
-    btn.addEventListener('click', function () {
-        for (let index = questions.length - 1; index >= 0; index--){
-            if (questions[index].classList.contains('question_active')) {
-                questions[index].classList.remove('question_active');
-                const question_position = document.querySelector('.question-position');
-                position = parseInt(question_position.textContent) - 1;
-                if (index === 0) {
-                    questions[questions.length - 1].classList.add('question_active');
-                    position = questions.length;
-                    
-                }
-                else {
-                    questions[index - 1].classList.add('question_active');
-                    
-                }
-                question_position.textContent = position;
-                break;
-            }
-        }
-
-    })
-
+function selectAnswer(answer) {
+    selectedAnswer = answer;
 }
 
-function checkAnswers(answers) {
-    
+function nextQuestion() {
+    if (selectedAnswer === null) {
+        alert("Please select an answer!");
+        return;
+    }
+
+    if (selectedAnswer === questions[currentQuestionIndex].correct_answer) {
+        score++;
+    }
+
+    selectedAnswer = null;
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showResults();
+    }
 }
-function getAnswers(position) {
-    let answer;
-    console.log("heey");
-    const answers = document.querySelectorAll('.options button');
-    answers.forEach((element) => {
-        element.addEventListener('click', () => {
-            answers.forEach((e) => {
-                e.classList.remove('answerActive');
-            })
-            element.classList.add('answerActive');
-            answer = element.dataset.value;
-            console.log(position)
-            console.log(userAnwers);
-            userAnwers[position.toString()] = answer;
-            console.log(userAnwers);
-            // console.log(answer);
-        })
-    })
+
+function showResults() {
+    document.getElementById("quiz-box").classList.add("hidden");
+    document.getElementById("result-box").classList.remove("hidden");
+
+    document.getElementById("score-text").innerText =
+        `You scored ${score} out of ${questions.length}`;
+}
+
+function restartQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    selectedAnswer = null;
+
+    document.getElementById("result-box").classList.add("hidden");
+    document.getElementById("quiz-box").classList.remove("hidden");
+
+    showQuestion();
 }
